444a2a0e7070fd88c69a1e8c0a6e69aed6d61e5b
Calindra Build Server <calindra@Calindra-Build-Server.local>
2025-09-25 21:07:33 -0300

    chore: publish eitri-ios-contracts

--- Package.swift
+++ Package.swift
@@ -3,9 +3,6 @@ import PackageDescription
 
 let package = Package(
     name: "EitriContracts",
-    platforms: [
-        .iOS(.v13)
-    ],
     products: [
         .library(
             name: "EitriContracts",
